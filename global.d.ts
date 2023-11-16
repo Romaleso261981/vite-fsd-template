9dc35854c3f75135e1147671c7591effcd7f5c61
Roman lesyo <ladiginscormag@gmail.com>
2023-11-16 07:55:40 +0200

    hello

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,5 @@
 type ExtendedWindow = Window &
   typeof globalThis & {
-    chaport?: {
-      q: (metodName: string) => void;
-    };
-    ym?: (id: number, hitType: string, url: string, option?: object) => void;
-    isTestEnvironment: boolean;
-    juicycoScoreApi?: any;
-    juicyLabConfig?: { completeButton: string };
-    jslabApi?: any;
     recaptchaVerifier?: any
     confirmationResult?: any
   };

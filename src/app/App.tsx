2b7a960928c06aa3b825abf6a5b9240ef11f694d
Roman lesyo <ladiginscormag@gmail.com>
2023-11-16 08:33:09 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 import Auth from '../pages/Auth/Auth';
-import Main from '../pages/Main/Main';
+// import Main from '../pages/Main/Main';
 // import AppRouter from './app-router';
 
 const App: React.FC = () => {

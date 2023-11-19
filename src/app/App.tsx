1ef5cba13eb3c50bc5ea3539ae5ccc1ae3475b50
Roman lesyo <ladiginscormag@gmail.com>
2023-11-19 15:37:30 +0200

    feat: added nofound page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import AuthPage from '../pages/AuthPage';
 import Main from '../pages/Main/Main';
+import { NotFound } from '../pages/NoFound/NoFound';
 import { routesLib } from '../shared/lib/index';
 
 const App = createBrowserRouter([
@@ -12,6 +13,10 @@ const App = createBrowserRouter([
   {
     path: routesLib.AUTH,
     element: <AuthPage />,
+  },
+  {
+    path: routesLib.AUTH,
+    element: <NotFound />,
   },
 ]);
 

a3f2d44ef8b9a9873113ba30736bc9cddbfa1051
Roman lesyo <ladiginscormag@gmail.com>
2023-11-23 14:54:15 +0200

    fix: i18nex

--- src/main.tsx
+++ src/main.tsx
@@ -10,19 +10,17 @@ import { RouterProvider } from 'react-router-dom';
 
 import App from './app/App';
 import store from './app/store';
-import initI18n from './i18n';
+import './shared/config/i18n/i18n';
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
-initI18n().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-      <MantineProvider theme={theme}>
-        <Notifications position="top-left" />
-        <RouterProvider router={App} />
-      </MantineProvider>
-    </Provider>,
-  );
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <MantineProvider theme={theme}>
+      <Notifications position="top-left" />
+      <RouterProvider router={App} />
+    </MantineProvider>
+  </Provider>,
+);

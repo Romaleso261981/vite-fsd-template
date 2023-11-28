a8ffaf3bd97fdf451fc3131a2630e18da2b728f4
Roman lesyo <ladiginscormag@gmail.com>
2023-11-28 13:40:56 +0200

    fix: added new routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,17 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import AuthPage from '../pages/Auth';
 import Main from '../pages/Main';
 import { NotFound } from '../pages/NoFound/NoFound';
-import { routesLib } from '../shared/lib/index';
 
-const App = createBrowserRouter([
-  {
-    path: routesLib.MAIN,
-    element: <Main />,
-  },
-  {
-    path: routesLib.AUTH,
-    element: <AuthPage />,
-  },
-  {
-    path: routesLib.NotFound,
-    element: <NotFound />,
-  },
-]);
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="auth" element={<AuthPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import '@mantine/core/styles.css';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
 
 import App from './app/App';
 import store from './app/store';
@@ -20,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <MantineProvider theme={theme}>
       <Notifications position="top-left" />
-      <RouterProvider router={App} />
+      <App />
     </MantineProvider>
   </Provider>,
 );

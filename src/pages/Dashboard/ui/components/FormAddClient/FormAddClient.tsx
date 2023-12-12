676b7acb99545977cbcf36cba609014136b3d8bc
Roman lesyo <ladiginscormag@gmail.com>
2023-12-12 16:52:49 +0200

    fix: added some styles a litle

--- src/pages/Dashboard/ui/components/FormAddClient/FormAddClient.tsx
+++ src/pages/Dashboard/ui/components/FormAddClient/FormAddClient.tsx
@@ -1,3 +1,50 @@
+import { Button, Flex, Input, Text, Title } from '@mantine/core';
+
+import s from './FormAddClient.module.css';
+
 export const FormAddClient = () => {
-  return <div>FormAddClient</div>;
+  return (
+    <Flex
+      mih={50}
+      gap="sm"
+      justify="flex-start"
+      align="flex-start"
+      direction="column"
+      wrap="wrap"
+    >
+      <Title order={1}>Клиенты</Title>
+      <Flex
+        mih={50}
+        gap="sm"
+        justify="flex-start"
+        align="flex-start"
+        direction="row"
+        wrap="wrap"
+      >
+        <Text size="xl">Всего: 18</Text>
+        <Button variant="filled">Button</Button>
+      </Flex>
+      <Flex
+        mih={50}
+        gap="sm"
+        justify="flex-start"
+        align="flex-start"
+        direction="row"
+        wrap="wrap"
+      >
+        <Input.Wrapper label="Телефон">
+          <Input placeholder="+380689478723" className={s.input} />
+        </Input.Wrapper>
+        <Input.Wrapper label="Никнейм">
+          <Input placeholder="example" className={s.input} />
+        </Input.Wrapper>
+        <Input.Wrapper label="ФИО">
+          <Input placeholder="Petro Petrov" className={s.input} />
+        </Input.Wrapper>
+        {/* <FileInput size="xs" placeholder="Input placeholder" />
+        <FileInput size="xs" placeholder="Input placeholder" />
+        <FileInput size="xs" placeholder="Input placeholder" /> */}
+      </Flex>
+    </Flex>
+  );
 };
--- src/pages/Dashboard/ui/components/Search/Search.tsx
+++ src/pages/Dashboard/ui/components/Search/Search.tsx
@@ -1,3 +1,13 @@
+import { Button, Flex, Input } from '@mantine/core';
+
+import s from './Search.module.css';
+
 export const Search = () => {
-  return <div>Search</div>;
+  return (
+    <Flex mih={50} gap="md" justify="center" align="center" direction="row" wrap="wrap">
+      <Input placeholder="example" className={s.input} />
+      {/* <FileInput size="xs" placeholder="Input placeholder" /> */}
+      <Button variant="filled">Сбросить</Button>
+    </Flex>
+  );
 };

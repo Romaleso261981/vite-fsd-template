e0b7a530cb87cec8c93d89240fba6ea755682efa
Roman lesyo <ladiginscormag@gmail.com>
2023-12-12 15:42:24 +0200

    fix: added object userData

--- src/shared/helpers/addDoc.ts
+++ src/shared/helpers/addDoc.ts
@@ -1,9 +1,15 @@
 import { addDoc, collection } from 'firebase/firestore';
 
 import { db } from '../../integations/firebase';
+import { UserData } from '../types/Types';
 
 export const setFirestoreData = (nickName: string, path: string) => {
+  const userData: UserData = {
+    nickName,
+    balans: '10',
+    LastName: 'Lesyo',
+  };
   const collectionRef = collection(db, path);
 
-  addDoc(collectionRef, { nickName });
+  addDoc(collectionRef, userData);
 };

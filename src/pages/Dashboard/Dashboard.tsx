2b5289806aed7c04fcb95b4128f5d85a0a04e55a
Roman lesyo <ladiginscormag@gmail.com>
2023-12-12 16:15:44 +0200

    feat: added Search and FormAddClient component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,33 +3,44 @@ import { Container, Flex } from '@mantine/core';
 import { Footer } from '../../features/components/Footer/Footer';
 import { Header } from '../../features/components/Header';
 
+import { FormAddClient } from './ui/components/FormAddClient/FormAddClient';
 import { NavbarMinimal } from './ui/components/Navbar/Navbar';
+import { Search } from './ui/components/Search/Search';
 import { TableSelection } from './ui/components/TableSelection/TableSelection';
 
 const Dashboard = () => {
   const demoProps = {
     bg: 'var(--mantine-color-blue-light)',
-    mt: 'md',
   };
 
   return (
-    <>
+    <Container size="xl" {...demoProps}>
       <Header />
-      <Container size="xl" {...demoProps}>
+
+      <Flex
+        mih={50}
+        gap="md"
+        justify="flex-start"
+        align="flex-start"
+        direction="row"
+        wrap="wrap"
+      >
+        <NavbarMinimal />
         <Flex
-          mih={50}
+          mih={40}
           gap="md"
           justify="flex-start"
           align="flex-start"
-          direction="row"
+          direction="column"
           wrap="wrap"
         >
-          <NavbarMinimal />
+          <FormAddClient />
+          <Search />
           <TableSelection />
         </Flex>
-      </Container>
+      </Flex>
       <Footer />
-    </>
+    </Container>
   );
 };
 
---
+++ src/pages/Dashboard/ui/components/FormAddClient/FormAddClient.tsx
@@ -0,0 +1,3 @@
+export const FormAddClient = () => {
+  return <div>FormAddClient</div>;
+};

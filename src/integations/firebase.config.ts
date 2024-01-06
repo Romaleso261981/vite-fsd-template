177773cb0b762e687c36fd8e8aa9fb0276f35eca
Roman lesyo <ladiginscormag@gmail.com>
2024-01-06 13:16:05 +0200

    fix: change firebaseConfig

--- src/integations/firebase.config.ts
+++ src/integations/firebase.config.ts
@@ -1,9 +1,18 @@
+// export const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   databaseURL: import.meta.env.VITE_databaseURL,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// };
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  databaseURL: import.meta.env.VITE_databaseURL,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: 'AIzaSyA0AdoQoblJrm_a9Qbh628U-Yt2-6DEKHQ',
+  authDomain: 'new-spa-b84ea.firebaseapp.com',
+  databaseURL: 'https://new-spa-b84ea-default-rtdb.firebaseio.com',
+  projectId: 'new-spa-b84ea',
+  storageBucket: 'new-spa-b84ea.appspot.com',
+  messagingSenderId: '326537037114',
+  appId: '1:326537037114:web:7b126f6bf625fe0b7dfc03',
 };

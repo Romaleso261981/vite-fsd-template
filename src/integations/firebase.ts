ffad37edc38097e77ee080d2371fca3f6bd4e1bf
Roman lesyo <ladiginscormag@gmail.com>
2024-01-07 17:06:21 +0200

    fix: some bag fix

--- src/integations/firebase.ts
+++ src/integations/firebase.ts
@@ -4,10 +4,15 @@ import { getFirestore } from 'firebase/firestore';
 
 import { firebaseConfig } from './firebase.config';
 
+console.log(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
+
+console.log(app);
 
 export const auth = getAuth(app);
 
+console.log(auth);
 export const db = getFirestore(app);
 
 export const provider = new GoogleAuthProvider();
